1a7ad7aa1b359b7178b3fb1feca6350d235b445a
Cyrus <86699936+cyrusae@users.noreply.github.com>
2026-03-27 11:07:01 -0700

    oops fix there too

--- terraform/02-storage-classes/outputs.tf
+++ terraform/02-storage-classes/outputs.tf
@@ -4,20 +4,20 @@
 
 output "critical" {
   description = "Name of the longhorn-critical storage class (3 replicas, one per node)."
-  value       = kubernetes_storage_class.longhorn_critical.metadata[0].name
+  value       = kubernetes_storage_class_v1.longhorn_critical.metadata[0].name
 }
 
 output "duplicate" {
   description = "Name of the longhorn-duplicate storage class (2 replicas, best-effort placement)."
-  value       = kubernetes_storage_class.longhorn_duplicate.metadata[0].name
+  value       = kubernetes_storage_class_v1.longhorn_duplicate.metadata[0].name
 }
 
 output "bulk" {
   description = "Name of the longhorn-bulk storage class (1 replica, most space available)."
-  value       = kubernetes_storage_class.longhorn_bulk.metadata[0].name
+  value       = kubernetes_storage_class_v1.longhorn_bulk.metadata[0].name
 }
 
 output "sticky" {
   description = "Name of the longhorn-sticky storage class (1 replica, strict-local, WaitForFirstConsumer)."
-  value       = kubernetes_storage_class.longhorn_sticky.metadata[0].name
+  value       = kubernetes_storage_class_v1.longhorn_sticky.metadata[0].name
 }
